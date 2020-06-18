f9a40a68f753ec2bbcc500895e8b1da2d6367fa5
Schrese <schreseholloway@gmail.com>
2020-06-18 16:08:25 -0400

    finished single_number, finished eating cookies based on info from the tk

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,11 +4,20 @@ Returns: an integer
 '''
 def eating_cookies(n):
     # Your code here
-
-    pass
-
-if __name__ == "__main__":
-    # Use the main function here to test out your implementation
-    num_cookies = 5
-
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    # pass 
+    if n <= 0: 
+        return 1 
+    if n == 1: 
+        return 1 
+    if n == 2: 
+        return 2 
+    if n == 3: 
+        return 4 
+    if n > 3: 
+        return eating_cookies(n - 3) + eating_cookies(n - 2) + eating_cookies(n - 1) 
+ 
+if __name__ == "__main__": 
+    # Use the main function here to test out your implementation 
+    num_cookies = 5 
+ 
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies") 
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,12 +2,50 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
+# with dictionaries
+def single_number(nums): # O(n)
+    # keep track of number of times an item occurs in input
+    counts = {}
+    # loop through input list O(n)
+    for num in nums:
+        # if item in counts
+        if num in counts:
+            # remove item
+            del counts[num]
+        # otherwise
+        else:
+            counts[num] = 1
+            # add item
+    for k, v in counts.items(): # O(1)
+        if v == 1:
+            return k
+# So. The reason that this is an 0(1) space complexity is because it is setting aside a CONSTANT amount of extra memory (with the counts dictionary or the s set). and the runtime is now O(n) because we're only looping through once (instead of twice which comes with the "count" list method)
+
+# with sets (like in lecture)
+# def single_number(arr):
+#     s = set() 
+#     # use either a dictionary or a set 
+#     # sets: holding onto unique elements 
+#     # loop through our arr
+#     for x in arr:
+#         # for each element
+#         # check if it is already in our set
+#         # if it is, then that's not our out-element-out 
+#         if x in s:
+#             # remove the element from our set 
+#             s.remove(x)
+#         else:
+#             s.add(x)
+#     # the odd-element-out will be the only element in the set 
+#     return list(s)[0]
+
+
+
     # I was thinking way too hard about this... I forgot about the count method until halfway through writing the first solution
-    for i in range(0, len(arr)):
-        if arr.count(i) == 1:
-            return i
+    # this solution is actually O(n^2) because the count method inherrently does another loop
+    # for i in range(0, len(arr)):
+    #     if arr.count(i) == 1:
+    #         return i
 
 
     # [3, 6, 4, 5, 6, 3, 4]
@@ -24,7 +62,7 @@ def single_number(arr):
     # we move to the next index
     # but if the value is not in the array we return that value
 
-    pass
+    # pass
 
 
 if __name__ == '__main__':
